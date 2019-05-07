aeaed6515e21f505a0ee68e81608e6304982b301
Anish S Nair <anish.vilayil.s@gmail.com>
2019-05-07 09:09:31 +0530

    Voice Over Enabled

--- app/src/main/java/com/logix/homeautomation/HomeControls.java
+++ app/src/main/java/com/logix/homeautomation/HomeControls.java
@@ -1,9 +1,14 @@
 package com.logix.homeautomation;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -17,17 +22,25 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class HomeControls extends AppCompatActivity {
 
-    final String serverUri = "tcp://192.168.31.79:1883";
+    final String serverUri = "tcp://192.168.31.236:1883";
     String clientId = "ExampleAndroidClient";
-    final String subscriptionTopic = "exampleAndroidTopic";
+    final String subscriptionTopic = "Topic";
     final String publishTopic = "Topic";
     final String publishMessagea= "a";
     final String publishMessageb = "b";
     final String publishMessagec = "c";
     final String publishMessaged = "d";
     private MqttAndroidClient mqttAndroidClient;
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    String VoiveControlMsg;
 
 
     @Override
@@ -40,6 +53,23 @@ public class HomeControls extends AppCompatActivity {
         Button off = (Button) findViewById(R.id.close);
         Button fanon = (Button) findViewById(R.id.fanOn);
         Button fanoff = (Button) findViewById(R.id.fanOff);
+
+
+
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+        // hide the action bar
+//        getActionBar().hide();
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
 
         fanoff.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,6 +176,80 @@ public class HomeControls extends AppCompatActivity {
             ex.printStackTrace();
         }
 
+    }
+
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+
+                    VoiveControlMsg=result.get(0);
+                    if(VoiveControlMsg.equals("light on") ){
+
+                        publishMessage(publishMessagea);
+                        Toast.makeText(getApplicationContext(),"LIGHT ON",Toast.LENGTH_LONG).show();
+                    }
+
+                    else if( VoiveControlMsg.equals("light off") ){
+
+                        publishMessage(publishMessageb);
+                        Toast.makeText(getApplicationContext(),"LIGHT OFF",Toast.LENGTH_LONG).show();
+
+                    }
+
+                    else if( VoiveControlMsg.equals("fan on") ){
+
+                        publishMessage(publishMessagec);
+                        Toast.makeText(getApplicationContext(),"FAN ON",Toast.LENGTH_LONG).show();
+
+                    }
+
+                    else if( VoiveControlMsg.equals("fan off") ){
+
+                        publishMessage(publishMessaged);
+                        Toast.makeText(getApplicationContext(),"FAN OFF",Toast.LENGTH_LONG).show();
+
+                    }
+
+
+
+
+
+                }
+                break;
+            }
+
+        }
     }
 
     private void addToHistory(String mainText){
